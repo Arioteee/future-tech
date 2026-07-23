7de57ea72c06efebb9ce50b473e4dedde812c1d6
Arioteee <smoke.tm.rec@gmail.com>
2026-07-23 23:09:52 +0700

    future/add rrd to render

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,15 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
 
-createRoot(document.getElementById("root")).render(<App />);
+import App from './App.jsx'
+import './index.css'
+
+const root = document.getElementById('root')
+
+createRoot(root).render(
+	<BrowserRouter>
+		<Routes>
+			<Route path='/' element={<App />} />
+		</Routes>
+	</BrowserRouter>,
+)
