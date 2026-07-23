fc2991602bca67601cc2310eb52a43a57e3e5f02
Arioteee <smoke.tm.rec@gmail.com>
2026-07-23 23:44:36 +0700

    feat/connect rrd

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
 
-import App from './App.jsx'
+import MainLayout from './components/layouts/MainLayout.jsx'
+import Resources from './pages/resources/Resources.jsx'
+import Podcasts from './pages/podcasts/Podcasts.jsx'
+import NotFound from './pages/notfound/NotFound.jsx'
+import Home from './pages/home/Home.jsx'
+import News from './pages/news/News.jsx'
+
 import './index.css'
 
 const root = document.getElementById('root')
@@ -9,7 +15,13 @@ const root = document.getElementById('root')
 createRoot(root).render(
 	<BrowserRouter>
 		<Routes>
-			<Route path='/' element={<App />} />
+			<Route path='/' element={<MainLayout />}>
+				<Route index element={<Home />} />
+				<Route path='news' element={<News />} />
+				<Route path='podcasts' element={<Podcasts />} />
+				<Route path='resources' element={<Resources />} />
+				<Route path='*' element={<NotFound />} />
+			</Route>
 		</Routes>
 	</BrowserRouter>,
 )
