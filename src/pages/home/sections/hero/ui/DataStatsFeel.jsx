d0d1517667e09316b8b79e0f8ec48c81140ca5bc
Arioteee <smoke.tm.rec@gmail.com>
2026-07-26 18:11:30 +0700

    feat/add new component for text field

---
+++ src/pages/home/sections/hero/ui/DataStatsFeel.jsx
@@ -0,0 +1,10 @@
+export const DataStatsFeel = ({ children }) => {
+	return (
+		<>
+			<p className='font-semibold text-[40px]'>
+				300<span className='text-brand'>+</span>
+			</p>
+			<p className='text-lg text-light-60'>{children}</p>
+		</>
+	)
+}
