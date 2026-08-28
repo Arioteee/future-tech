3d9233457f981d3bbc77a28d6fd25b80b5b6bf21
Arioteee <smoke.tm.rec@gmail.com>
2026-08-29 05:02:49 +0700

    fix/edit margin bug

--- src/pages/home/sections/hero/ui/DataStatsFeel.jsx
+++ src/pages/home/sections/hero/ui/DataStatsFeel.jsx
@@ -1,7 +1,7 @@
 export const DataStatsFeel = ({ children }) => {
 	return (
 		<>
-			<p className='font-semibold text-[40px]'>
+			<p className='font-semibold text-[40px] mb-6'>
 				300<span className='text-brand'>+</span>
 			</p>
 			<p className='text-lg text-light-60'>{children}</p>
