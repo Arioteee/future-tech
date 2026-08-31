c4e1550846531cd33339efaa1927ee2b10cc7f89
Arioteee <smoke.tm.rec@gmail.com>
2026-08-31 22:31:00 +0700

    feat/export iconMap for future section

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,7 +1,13 @@
 import { DividedSemicircle, PlusCross, QuadrantCircle } from './Icons'
+import { CirclePlus, BoxBlogs } from './Icons'
 
 export const iconMap = {
 	dividedSemiCircle: DividedSemicircle,
 	plusCross: PlusCross,
 	quadrantCircle: QuadrantCircle,
+}
+
+export const iconMapFuture = {
+	circlePlus: CirclePlus,
+	boxBlogs: BoxBlogs,
 }
