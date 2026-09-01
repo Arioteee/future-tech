318c0e5898ad66f1df16face27bd9f082f3f7142
Arioteee <smoke.tm.rec@gmail.com>
2026-09-01 21:03:44 +0700

    feat/future section connect

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,12 @@
+import { Futures } from './sections/features/Futures'
 import { Hero } from './sections/hero/Hero'
 
 export default function Home() {
 	return (
 		<div>
 			<Hero />
+
+			<Futures />
 		</div>
 	)
 }
