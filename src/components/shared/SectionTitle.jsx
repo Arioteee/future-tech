f409ee9071a1a1a76356467a45cfd5fe4fa036a7
Arioteee <smoke.tm.rec@gmail.com>
2026-09-02 22:31:40 +0700

    fix/pre edit styles

--- src/components/shared/SectionTitle.jsx
+++ src/components/shared/SectionTitle.jsx
@@ -1,14 +1,23 @@
-export const SectionTitle = ({ title, description }) => {
+import { CustomContainer } from '@/components/shared/CustomContainer'
+
+export const SectionTitle = ({ title, description, children }) => {
 	return (
 		<div className='bg-dark-10 py-30 border-y border-dark-15 max-xxl:py-20 max-md:py-10'>
-			<div className='container-custom'>
-				<span className='py-1.5 px-2.5 rounded-sm bg-dark-20 text-xl max-xxl:text-base max-md:py-1 max-md:px-2'>
-					{description}
-				</span>
-				<h2 className='mt-4 text-[58px] leading-[130%] max-xxl:text-[44px] max-xxl:font-medium max-md:text-3xl'>
-					{title}
-				</h2>
-			</div>
+			<CustomContainer>
+				<div className='flex items-center justify-between'>
+					<div>
+						<span className='rounded-sm bg-dark-20 py-1 px-2 text-xs sm:text-sm md:py-1.5 md:px-2.5 md:text-base lg:text-lg xl:text-xl'>
+							{description}
+						</span>
+
+						<h2 className='font-family-second mt-4 leading-[130%] text-2xl md:text-3xl lg:text-[44px] xl:text-[58px] font-medium text-wrap'>
+							{title}
+						</h2>
+					</div>
+
+					{children}
+				</div>
+			</CustomContainer>
 		</div>
 	)
 }
