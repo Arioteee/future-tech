26612b661c84f9e97747fd630218802c68ad8e3b
Arioteee <smoke.tm.rec@gmail.com>
2026-09-02 22:32:15 +0700

    fix/fixed padding bug

--- src/pages/home/sections/features/ui/FutureCardBlock.jsx
+++ src/pages/home/sections/features/ui/FutureCardBlock.jsx
@@ -1,6 +1,6 @@
 export const FutureCardBlock = ({ children }) => {
 	return (
-		<div className='grid grid-cols-2 grid-rows-2 gap-7.5 p-20 max-md:grid-rows-4 max-md:grid-cols-1 max-xxl:p-0 max-xxl:gap-5 max-md:gap-2.5 max-xxl:px-4'>
+		<div className='grid grid-cols-2 grid-rows-2 gap-7.5 p-20 pr-0 max-md:grid-rows-4 max-md:grid-cols-1 max-xxl:p-0 max-xxl:gap-5 max-md:gap-2.5 max-xxl:pl-4'>
 			{children}
 		</div>
 	)
