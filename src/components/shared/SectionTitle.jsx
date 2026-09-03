1235b793ed740af6d61bab811a5e2391014e2cff
Arioteee <smoke.tm.rec@gmail.com>
2026-09-03 22:52:17 +0700

    feat/new props styles

--- src/components/shared/SectionTitle.jsx
+++ src/components/shared/SectionTitle.jsx
@@ -1,16 +1,16 @@
 import { CustomContainer } from '@/components/shared/CustomContainer'
 
-export const SectionTitle = ({ title, description, children }) => {
+export const SectionTitle = ({ title, description, styles, children }) => {
 	return (
 		<div className='bg-dark-10 py-30 border-y border-dark-15 max-xxl:py-20 max-md:py-10'>
 			<CustomContainer>
-				<div className='flex items-center justify-between'>
+				<div className={`flex items-center justify-between ${styles}`}>
 					<div>
 						<span className='rounded-sm bg-dark-20 py-1 px-2 text-xs sm:text-sm md:py-1.5 md:px-2.5 md:text-base lg:text-lg xl:text-xl'>
 							{description}
 						</span>
 
-						<h2 className='font-family-second mt-4 leading-[130%] text-2xl md:text-3xl lg:text-[44px] xl:text-[58px] font-medium text-wrap'>
+						<h2 className='font-family-second mt-4 leading-[130%] text-2xl md:text-3xl lg:text-[44px] 2xl:text-[58px] font-medium text-wrap'>
 							{title}
 						</h2>
 					</div>
